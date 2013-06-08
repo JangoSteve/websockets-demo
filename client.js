30e347a0683a45a4c7db9e06124d486a6ead6983
Steve Schwartz <steve@alfajango.com>
2013-06-08 12:19:31 -0400

    Basic websocket communication between client and server.

---
+++ client.js
@@ -0,0 +1,16 @@
+var connection = new WebSocket('ws://localhost:8080');
+
+// When the connection is open, send some data to the server
+connection.onopen = function () {
+  connection.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+connection.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+  console.log('Server: ' + e.data);
+};
