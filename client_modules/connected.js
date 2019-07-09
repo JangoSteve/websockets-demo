73574f9adf0635c2d899054a0b31ced6b2da3974
Steve Schwartz <steve@alfajango.com>
2019-07-09 12:56:19 -0400

    Show local network IP address in app for live demo.

--- client_modules/connected.js
+++ client_modules/connected.js
@@ -2,5 +2,5 @@ msg.install("connected.js", "https://github.com/JangoSteve/websockets-demo/blob/
 
 msg.connected = function(data) {
   connection.id = data.id;
-  msg.log('You are connected as client ' + data.id + '.');
+  msg.log('You are connected as client ' + data.id + '. Local network IP address: ' + data.localIps.join(', '));
 };
--- server.js
+++ server.js
@@ -3,7 +3,31 @@ var WebSocketServer = require('ws').Server,
     connections = {},
     connect = require('connect'),
     clientServer = connect.createServer(connect.static(__dirname)).listen(process.env.PORT || 8081),
-    wss = new WebSocketServer({server: clientServer});
+    wss = new WebSocketServer({server: clientServer}),
+    os = require('os'),
+    ifaces = os.networkInterfaces(),
+    localIps = [];
+
+// Adapted from https://stackoverflow.com/a/8440736/366381
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      localIps.push(iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      localIps.push(iface.address);
+    }
+    ++alias;
+  });
+});
 
 wss.on('connection', function(ws) {
   i++;
@@ -27,7 +51,8 @@ wss.on('connection', function(ws) {
   // Connected event
   ws.send(JSON.stringify({
     type: 'connected',
-    id: ws.id
+    id: ws.id,
+    localIps: localIps
   }));
 
   // Connection events
